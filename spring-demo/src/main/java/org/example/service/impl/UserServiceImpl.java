12950ae7c87ad30ac62600b7a3468a8a054d8da0
xiaoliu <1713612859@qq.com>
2023-09-04 11:55:53 +0800

    Java  ThreadLocal-demo

--- spring-demo/src/main/java/org/example/service/impl/UserServiceImpl.java
+++ spring-demo/src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -19,5 +19,16 @@ public class UserServiceImpl implements UserService {
 	 public void toEat(String clazzName) throws ClassNotFoundException {
 		  Class<?> clazz = Class.forName(clazzName);
 		  System.out.println(clazz.getName());
+		  Object x = null;
+		  try {
+				x = clazz.newInstance();
+		  }
+		  catch (InstantiationException e) {
+				throw new RuntimeException(e);
+		  }
+		  catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+		  }
+		  System.out.println(x);
 	 }
 }
